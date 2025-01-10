c096bc3c2d087a1c35aace10812b78a753479190
zhangbingbin <bingbinzhang@whale.im>
2025-01-10 14:59:15 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: './', // Set the base path to current directory
   plugins: [react()],
 })
